635e5ab6c317c9f39b9b7fa06c7a07c0319ae91e
MeesterMarcus <lorenzanamarcus@gmail.com>
2024-01-04 16:14:58 -0600

    adding proper logging

--- src/main/java/com/marcuslorenzana/imageobjectdetector/services/ImageService.java
+++ src/main/java/com/marcuslorenzana/imageobjectdetector/services/ImageService.java
@@ -3,6 +3,8 @@ package com.marcuslorenzana.imageobjectdetector.services;
 import com.marcuslorenzana.imageobjectdetector.models.ImageMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +18,23 @@ public class ImageService {
     @Value("${app.IMAGGA_API_AUTH}")
     private String imaggaAuth;
 
+    Logger logger = LoggerFactory.getLogger(ImageService.class);
+
     public List<ImageMetadata> getAllImages(String objects) {
-        System.out.println(imaggaBaseUrl);
-        System.out.println(imaggaAuth);
-        System.out.println(objects);
+        logger.info(imaggaBaseUrl);
+        logger.info(imaggaAuth);
+        logger.info(objects);
         List<ImageMetadata> images = new ArrayList<>();
         return images;
     }
 
     public ImageMetadata getImageById(String itemId) {
-        System.out.println(itemId);
+        logger.info(itemId);
         return null;
     }
 
     public ImageMetadata createImage(ImageMetadata image) {
-        System.out.println(image);
+        logger.info(image.toString());
         return image;
     }
 }
